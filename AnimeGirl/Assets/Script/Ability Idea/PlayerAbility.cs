3f66caef7dfd85462dab76791da41038bac17fa7
KAISER\Vladi <gabrielrocha2007brasil@gmail.com>
2025-11-18 18:17:42 -0800

    power up and waves done.

---
+++ AnimeGirl/Assets/Script/Ability Idea/PlayerAbility.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class PlayerAbilities : MonoBehaviour
+{
+    void Update()
+    {
+        // input handling
+        if (Input.GetMouseButtonDown(1))
+        {
+            TryUseAbility(AbilityType.Meteor);
+        }
+    }
+
+    // Added missing method definition
+    private void TryUseAbility(AbilityType abilityType)
+    {
+        // TODO: Implement ability usage logic here
+        Debug.Log($"Trying to use ability: {abilityType}");
+    }
+}
---
+++ AnimeGirl/Assets/Script/PowerUpsMANAGER/LevelUpCard.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelUpCard : MonoBehaviour
+{
+    public Text titleText;
+    public Text descriptionText;
+    public Text countText;    // optional small text to show duplicate count
+    public Image iconImage;
+    public Button button;
+
+    private Action onClick;
+
+    public void Setup(string title, string description, Sprite icon, int count, Action onClickCallback)
+    {
+        if (titleText != null) titleText.text = title;
+        if (descriptionText != null) descriptionText.text = description;
+        if (countText != null)
+        {
+            countText.gameObject.SetActive(count > 1);
+            countText.text = $"x{count}";
+        }
+
+        if (iconImage != null) iconImage.sprite = icon;
+
+        onClick = onClickCallback;
+        if (button != null)
+        {
+            button.onClick.RemoveAllListeners();
+            button.onClick.AddListener(() => onClick?.Invoke());
+        }
+    }
+}

3a8e5d78721d0a15f60b25bb58c198dbc2e81995
MUKILTEO\1630107 <1630107@mukilteo.wednet.edu>
2025-11-25 09:57:45 -0800

    Done with Damage and Death

--- AnimeGirl/Assets/Abigail Code/Enemy.cs
+++ AnimeGirl/Assets/Abigail Code/Enemy.cs
@@ -2,20 +2,30 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float health;
+    public float maxHealth;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        health = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        DestroyEnemy();
     }
     
     public void TakeDamage(float damage)
     {
+        health -= damage;
+    }
 
+    void DestroyEnemy()
+    {
+        if (health <= 0f)
+        {
+            Destroy(gameObject);
+        }
     }
 }

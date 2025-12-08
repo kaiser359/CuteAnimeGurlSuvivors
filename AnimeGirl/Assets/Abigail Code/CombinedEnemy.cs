fde8b755b1f27536b3c907679d04ffd486e8c4f2
MUKILTEO\1630107 <1630107@mukilteo.wednet.edu>
2025-12-08 10:07:15 -0800

    Combined enemy is not really working but kinda is

--- AnimeGirl/Assets/Abigail Code/CombinedEnemy.cs
+++ AnimeGirl/Assets/Abigail Code/CombinedEnemy.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class CombinedEnemy : MonoBehaviour
 {
+    public EnemyAttack melee;
+    public EnemyRangedAttack ranaged;
+    public float DetectionRadius = 10f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Update()
     {
-        
+        ChangeAttackMethod();
     }
-
-    // Update is called once per frame
-    void Update()
+    void ChangeAttackMethod()
     {
-        
+        if(Vector2.Distance(transform.position, GameObject.FindWithTag("Player").transform.position) < DetectionRadius)
+        {
+            ranaged.enabled = false;
+            melee.enabled = true;
+
+        }
+        else 
+        {
+            ranaged.enabled = true;
+            melee.enabled = false;
+        }
     }
 }

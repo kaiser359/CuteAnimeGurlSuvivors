e43b7b00ed5a04b9971c74d281cfd4855f75496e
MUKILTEO\1720941 <1720941>
2025-12-18 08:58:28 -0800

    more yummms

--- AnimeGirl/Assets/Script/Online/EnemyManager.cs
+++ AnimeGirl/Assets/Script/Online/EnemyManager.cs
@@ -23,7 +23,7 @@ public class EnemyManager : MonoBehaviour
 
     // Wave management
     public int currentWave = 1;
-    private int enemiesPerWave = 2; // Start with 10 enemies in wave 1
+    private int enemiesPerWave = 5; // Start with 10 enemies in wave 1
     [SerializeField]private int enemiesSpawnedInWave = 0;
    [SerializeField] private int currentEnemiesAlive = 0;
 
@@ -36,7 +36,7 @@ public class EnemyManager : MonoBehaviour
     public float MaxSpawnDistance = 30f; // Max Spawn Distance Away From Player
 
     // Difficulty scaling (adjust as needed)
-    private const int ENEMIES_INCREASE_PER_WAVE = 3; //const int, and other functions make the game run more smoothly, i recommend. 
+    private const int ENEMIES_INCREASE_PER_WAVE = 1; //const int, and other functions make the game run more smoothly, i recommend. 
     private const float SPAWN_RATE_INCREASE_PER_WAVE = 0.2f;
     void Start()
     {

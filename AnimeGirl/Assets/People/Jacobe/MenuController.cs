159bd05e69909835fbfb9699ec7e886d6e5c2853
MUKILTEO\1720941 <1720941>
2026-01-05 08:59:18 -0800

    no message

--- AnimeGirl/Assets/People/Jacobe/MenuController.cs
+++ AnimeGirl/Assets/People/Jacobe/MenuController.cs
@@ -1,107 +1,158 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
-using System.Xml.Linq;
 
-public class MenuController : MonoBehaviour
+public class MenuController
 {
     public Button LoadGameDialog_YesBtn;
 
-    [Header("Volume Setting")]
-    [SerializeField] private TMP_Text volumeTextValue = null;
-    [SerializeField] private Slider volumeSlider = null;
-    [SerializeField] private float defaultVolume = 1.0f;
+    // Volume Setting
+    private readonly TMP_Text volumeTextValue;
+    private readonly Slider volumeSlider;
+    private readonly float defaultVolume = 1.0f;
 
-    [Header("Gameplay Settings")]
-    [SerializeField] private TMP_Text SensitvityTextValue = null;
-    [SerializeField] private Slider SensitvitySlider = null;
-    [SerializeField] private int defaultSensitvity = 4;
+    // Gameplay Settings
+    private readonly TMP_Text SensitvityTextValue;
+    private readonly Slider SensitvitySlider;
+    private readonly int defaultSensitvity = 4;
     public int mainSensitivity = 4;
 
-    [Header("Toggle settings")]
-    [SerializeField] private Toggle invertYToggle = null;
+    // Toggle settings
+    private readonly Toggle invertYToggle;
 
-    [Header("Graphics Settings")]
-    [SerializeField] private Slider brightnessSlider = null;
-    [SerializeField] private TMP_Text brightnessTextValue = null;
-    [SerializeField] private float defaultBrightness = 1;
+    // Graphics Settings
+    private readonly Slider brightnessSlider;
+    private readonly TMP_Text brightnessTextValue;
+    private readonly float defaultBrightness = 1;
 
-    [Space(10)]
-    [SerializeField] private TMP_Dropdown qualityDropdown;
-    [SerializeField] private Toggle fullScreenToggle;
+    private readonly TMP_Dropdown qualityDropdown;
+    private readonly Toggle fullScreenToggle;
 
     private int _qualityLevel;
     private bool _isFullScreen;
     private float _brightnessLevel;
 
+    // Confirmation
+    private readonly GameObject comfirmationPrompt;
+    private float _confirmationTimer;
+    private bool _confirmationActive;
 
-    [Header("Confirmation")]
-    [SerializeField] private GameObject comfirmationPrompt = null;
-
-    [Header("Worlds To Load")]
+    // Worlds To Load
     public string _newGameWorld;
     private string WorldToLoad;
-    [SerializeField] private GameObject noSavedGameDialog = null;
+    private readonly GameObject noSavedGameDialog;
 
-    [Header("Resolution Dropdowns")]
+    // Resolution Dropdowns
     public TMP_Dropdown resolutionDropdown;
     private Resolution[] resolutions;
 
     public float volume;
 
     public chooseChar chooseChar;
-    //public TMP_Text volumeText;
 
-    private void Start()
+    public Button LoadGameBtn;
+
+    // Constructor - supply dependencies from a MonoBehaviour or factory
+    public MenuController(
+        TMP_Text volumeTextValue,
+        Slider volumeSlider,
+        TMP_Text sensitText,
+        Slider sensitSlider,
+        Toggle invertYToggle,
+        Slider brightnessSlider,
+        TMP_Text brightnessTextValue,
+        TMP_Dropdown qualityDropdown,
+        Toggle fullScreenToggle,
+        GameObject comfirmationPrompt,
+        GameObject noSavedGameDialog,
+        TMP_Dropdown resolutionDropdown,
+        chooseChar chooseChar)
+    {
+        this.volumeTextValue = volumeTextValue;
+        this.volumeSlider = volumeSlider;
+        this.SensitvityTextValue = sensitText;
+        this.SensitvitySlider = sensitSlider;
+        this.invertYToggle = invertYToggle;
+        this.brightnessSlider = brightnessSlider;
+        this.brightnessTextValue = brightnessTextValue;
+        this.qualityDropdown = qualityDropdown;
+        this.fullScreenToggle = fullScreenToggle;
+        this.comfirmationPrompt = comfirmationPrompt;
+        this.noSavedGameDialog = noSavedGameDialog;
+        this.resolutionDropdown = resolutionDropdown;
+        this.chooseChar = chooseChar;
+    }
+
+    // Call from a MonoBehaviour Start to initialize UI state
+    public void Initialize()
     {
         resolutions = Screen.resolutions;
-        resolutionDropdown.ClearOptions();
-
-        List<string> options = new List<string>();
-
-        int currentResolutionIndex = 0;
-
-        for (int i = 0; i < resolutions.Length; i++)
+        if (resolutionDropdown != null)
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(option);
+            resolutionDropdown.ClearOptions();
 
-            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            List<string> options = new List<string>();
+
+            int currentResolutionIndex = 0;
+
+            for (int i = 0; i < resolutions.Length; i++)
             {
-                currentResolutionIndex = i;
+                string option = resolutions[i].width + " x " + resolutions[i].height;
+                options.Add(option);
+
+                if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+                {
+                    currentResolutionIndex = i;
+                }
+            }
+
+            resolutionDropdown.AddOptions(options);
+            resolutionDropdown.value = currentResolutionIndex;
+            resolutionDropdown.RefreshShownValue();
+        }
+
+        if (volumeSlider != null)
+            volumeSlider.value = PlayerPrefs.GetFloat("volume");
+    }
+
+    // Call each frame from a MonoBehaviour Update to keep UI synced
+    public void Tick(float deltaTime)
+    {
+        // handle confirmation box timing
+        if (_confirmationActive)
+        {
+            _confirmationTimer -= deltaTime;
+            if (_confirmationTimer <= 0f)
+            {
+                if (comfirmationPrompt != null)
+                    comfirmationPrompt.SetActive(false);
+                _confirmationActive = false;
             }
         }
 
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
-
-        volumeSlider.value = PlayerPrefs.GetFloat("volume");
-    }
-
-    private void Update()
-    {
+        // update volume UI and persist
         PlayerPrefs.SetFloat("volume", volume);
-        volumeTextValue.text = Mathf.Round(volume * 100) + "%";
+        if (volumeTextValue != null)
+            volumeTextValue.text = Mathf.Round(volume * 100) + "%";
     }
 
     public void SetResolution(int resolutionIndex)
     {
+        if (resolutions == null || resolutionIndex < 0 || resolutionIndex >= resolutions.Length) return;
         Resolution resolution = resolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
     public void NewGameDialogYes()
-    {if (chooseChar.chosen)
+    {
+        if (chooseChar != null && chooseChar.chosen)
         {
             _newGameWorld = "SampleScene 2";
         }
         else
         {
-            
             _newGameWorld = "SampleScene";
         }
         SceneManager.LoadScene(_newGameWorld);
@@ -112,12 +163,12 @@ public class MenuController : MonoBehaviour
         if (PlayerPrefs.HasKey("SavedWorld"))
         {
             WorldToLoad = PlayerPrefs.GetString("SavedWorld");
-            //PlayerPrefs.SetString("SavedLevel", whateveryourlevelis)
             SceneManager.LoadScene(WorldToLoad);
         }
         else
         {
-            noSavedGameDialog.SetActive(true);
+            if (noSavedGameDialog != null)
+                noSavedGameDialog.SetActive(true);
         }
     }
 
@@ -134,36 +185,32 @@ public class MenuController : MonoBehaviour
     public void VolumeApply()
     {
         PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
-        StartCoroutine(ConfirmationBox());
+        ShowConfirmation(2f);
     }
 
     public void SetSensitvity(float sensitvity)
     {
         mainSensitivity = Mathf.RoundToInt(sensitvity);
-        SensitvityTextValue.text = Mathf.Round(sensitvity * 10) + "%";
+        if (SensitvityTextValue != null)
+            SensitvityTextValue.text = Mathf.Round(sensitvity * 10) + "%";
     }
 
     public void GameplayApply()
     {
-        if (invertYToggle.isOn)
+        if (invertYToggle != null)
         {
-            PlayerPrefs.SetInt("masterInvertY", 1);
-            //invertY
-        }
-        else
-        {
-            PlayerPrefs.SetInt("masterInvertY", 0);
-            //Not invert
+            PlayerPrefs.SetInt("masterInvertY", invertYToggle.isOn ? 1 : 0);
         }
 
         PlayerPrefs.SetFloat("masterSensitvity", mainSensitivity);
-        StartCoroutine(ConfirmationBox());
+        ShowConfirmation(2f);
     }
 
     public void SetBrightness(float brightness)
     {
         _brightnessLevel = brightness;
-        brightnessTextValue.text = brightness.ToString("0.0");
+        if (brightnessTextValue != null)
+            brightnessTextValue.text = brightness.ToString("0.0");
     }
 
     public void SetFullScreen(bool isFullScreen)
@@ -179,7 +226,6 @@ public class MenuController : MonoBehaviour
     public void GraphicsApply()
     {
         PlayerPrefs.SetFloat("masterBrightness", _brightnessLevel);
-        // change your brightness with post processing
 
         PlayerPrefs.SetInt("masterQuality", _qualityLevel);
         QualitySettings.SetQualityLevel(_qualityLevel);
@@ -187,55 +233,51 @@ public class MenuController : MonoBehaviour
         PlayerPrefs.SetInt("masterFullscreen", (_isFullScreen ? 1 : 0));
         Screen.fullScreen = _isFullScreen;
 
-        StartCoroutine(ConfirmationBox());
+        ShowConfirmation(2f);
     }
 
     public void ResetButton(string MenuType)
     {
-
         if (MenuType == "Graphics")
         {
-            //reset brightness value
-            brightnessSlider.value = defaultBrightness;
-            brightnessTextValue.text = defaultBrightness.ToString("0.0");
+            if (brightnessSlider != null) brightnessSlider.value = defaultBrightness;
+            if (brightnessTextValue != null) brightnessTextValue.text = defaultBrightness.ToString("0.0");
 
-            qualityDropdown.value = 1;
+            if (qualityDropdown != null) qualityDropdown.value = 1;
             QualitySettings.SetQualityLevel(1);
 
-            fullScreenToggle.isOn = false;
+            if (fullScreenToggle != null) fullScreenToggle.isOn = false;
             Screen.fullScreen = false;
 
             Resolution currentResolution = Screen.currentResolution;
             Screen.SetResolution(currentResolution.width, currentResolution.height, Screen.fullScreen);
-            resolutionDropdown.value = resolutions.Length;
+            if (resolutionDropdown != null && resolutions != null) resolutionDropdown.value = resolutions.Length;
             GraphicsApply();
         }
 
         if (MenuType == "Audio")
         {
             AudioListener.volume = defaultVolume;
-            volumeSlider.value = defaultVolume;
-            volumeTextValue.text = defaultVolume.ToString("0.0");
+            if (volumeSlider != null) volumeSlider.value = defaultVolume;
+            if (volumeTextValue != null) volumeTextValue.text = defaultVolume.ToString("0.0");
             VolumeApply();
         }
 
         if (MenuType == "Gameplay")
         {
-            SensitvityTextValue.text = defaultSensitvity.ToString("0");
-            SensitvitySlider.value = defaultSensitvity;
+            if (SensitvityTextValue != null) SensitvityTextValue.text = defaultSensitvity.ToString("0");
+            if (SensitvitySlider != null) SensitvitySlider.value = defaultSensitvity;
             mainSensitivity = defaultSensitvity;
-            invertYToggle.isOn = false;
+            if (invertYToggle != null) invertYToggle.isOn = false; 
             GameplayApply();
         }
     }
 
-    public Button LoadGameBtn;
-
-
-    public IEnumerator ConfirmationBox()
+    // Replaces coroutine -- caller must call Tick(deltaTime) each frame
+    public void ShowConfirmation(float durationSeconds)
     {
-        comfirmationPrompt.SetActive(true);
-        yield return new WaitForSeconds(2);
-        comfirmationPrompt.SetActive(false);
+        if (comfirmationPrompt != null) comfirmationPrompt.SetActive(true);
+        _confirmationTimer = durationSeconds;
+        _confirmationActive = true;
     }
 }
